e031d3f960981f1b1e67ab6d045a787980e9ceda
RizwanRumi <rizwan.cse33@gmail.com>
2017-08-21 20:04:55 +0600

    update

--- S3LabTestWebApi/S3LabTestWebApi/BL/SyllabusManager.cs
+++ S3LabTestWebApi/S3LabTestWebApi/BL/SyllabusManager.cs
@@ -14,33 +14,23 @@ namespace S3LabTestWebApi.BL
         public int SaveSyllabus(SyllabusDetailsModel slb)
         {
             int id = 0;
-            tblSyllabus sb = new tblSyllabus();
-           // sb.colSyllabusId = slb.SyllabusId;
-            //sb.colSyllabusName = slb.SyllabusName;
-            //sb.colTradeId = slb.TradeId;
-            //sb.colLevelId = slb.LevelId;
-            //sb.colSyllabusDocUrl = slb.SyllabusDocUrl;
-            //sb.colTestPlanUrl = slb.TestPlanUrl;
-            //sb.colDevelopmentOfficer = slb.DevelopmentOfficer;
-            //sb.colManager = slb.Manager;
-            //sb.colUploadBy = slb.UploadBy;
-            //sb.colUploadDt = slb.UploadDt;
-            //sb.colActiveDt = slb.ActiveDt;
-            //sb.colSt = Convert.ToBoolean(slb.Status);
+            tblSyllabu sb = new tblSyllabu();
 
-            sb.colSyllabusName = "Test";
-            sb.colTradeId = 2;
-            sb.colLevelId = 3;
-            sb.colSyllabusDocUrl = "DocTest.docx";
-            sb.colTestPlanUrl = "PlanTest.xml";
-            sb.colDevelopmentOfficer = "Rizwan";
-            sb.colManager = "Rumi";
-            sb.colUploadBy = 2;
-            sb.colUploadDt = DateTime.Now;
-            sb.colActiveDt = DateTime.Now;
-            sb.colSt = true;
+            sb.colSyllabusId = slb.SyllabusId;
+            sb.colSyllabusName = slb.SyllabusName;
+            sb.colTradeId = slb.TradeId;
+            sb.colLevelId = slb.LevelId;
+            sb.colSyllabusDocUrl = slb.SyllabusDocUrl;
+            sb.colTestPlanUrl = slb.TestPlanUrl;
+            sb.colDevelopmentOfficer = slb.DevelopmentOfficer;
+            sb.colManager = slb.Manager;
+            sb.colUploadBy = slb.UploadBy;
+            sb.colUploadDt = slb.UploadDt;
+            sb.colActiveDt = slb.ActiveDt;
+            sb.colSt = Convert.ToBoolean(slb.Status);
+
             
-            _dbContext.tblSyllabus1.Add(sb);
+            _dbContext.tblSyllabus.Add(sb);
 
             _dbContext.SaveChanges();
 
